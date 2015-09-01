d4aa72463c97070dbf2d88b0e7f46c619d5d7e34
huangjinfu <xfujohn@xfu.John>
2015-09-01 14:44:04 +0800

    修改统计选择界面的UI

--- src/cn/hjf/gollumaccount/activity/StatisticSelectActivity.java
+++ src/cn/hjf/gollumaccount/activity/StatisticSelectActivity.java
@@ -3,6 +3,8 @@ package cn.hjf.gollumaccount.activity;
 import cn.hjf.gollumaccount.FragmentIdConsts;
 import cn.hjf.gollumaccount.R;
 import cn.hjf.gollumaccount.activity.MainActivity;
+import cn.hjf.gollumaccount.fragment.CommonHeaderFragment;
+import cn.hjf.gollumaccount.fragment.CommonHeaderFragment.HEAD_TYPE;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,18 +22,35 @@ import android.widget.Button;
  * @author huangjinfu
  * 
  */
-public class StatisticSelectActivity extends BaseActivity {
+public class StatisticSelectActivity extends BaseActivity implements CommonHeaderFragment.ICallback{
 
     private Button mByItemButton;
     private Button mByMonthButton;
+    /**
+     * 顶部标题栏
+     */
+    private CommonHeaderFragment mTitleFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_analyse);
+        
+        initTitle();
         initView();
         initValue();
         initEvent();
+    }
+    
+    /**
+     * 初始化顶部导航栏
+     */
+    @Override
+    public void initTitle() {
+        mTitleFragment = (CommonHeaderFragment) mFragmentManager.findFragmentById(R.id.title_statistic_select);
+        mTitleFragment.setHeadBtnType(HEAD_TYPE.LEFT_BACK_TEXT,HEAD_TYPE.RIGHT_NULL);
+        mTitleFragment.setHeadText(R.string.title_back, R.string.title_analyse, null);
+        mTitleFragment.setCallback(this);
     }
 
     /**
@@ -70,5 +89,14 @@ public class StatisticSelectActivity extends BaseActivity {
                 StatisticSelectActivity.this.startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onLeftClick() {
+        finish();
+    }
+
+    @Override
+    public void onRightClick() {
     }
 }
