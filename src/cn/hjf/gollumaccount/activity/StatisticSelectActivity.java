7ddd7066a748b26a72c6b5e8eae527936e11c23a
huangjinfu <xfujohn@xfu.John>
2015-09-09 12:52:32 +0800

    按类型统计界面，字符串整合进资源文件

--- src/cn/hjf/gollumaccount/activity/StatisticSelectActivity.java
+++ src/cn/hjf/gollumaccount/activity/StatisticSelectActivity.java
@@ -4,22 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import cn.hjf.gollumaccount.R;
-import cn.hjf.gollumaccount.activity.MainActivity;
 import cn.hjf.gollumaccount.adapter.StatisticSelectMenuAdapter;
 import cn.hjf.gollumaccount.fragment.CommonHeaderFragment;
 import cn.hjf.gollumaccount.fragment.CommonHeaderFragment.HEAD_TYPE;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
 import android.widget.ListView;
 
 /**
